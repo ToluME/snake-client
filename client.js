4a575a45fceb83790728baed556136602fb3f91f
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-17 15:49:10 -0600

    Up movement added and commented out

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: TME");
+    // conn.write("Move: up");
   });
   conn.on("data", (data) => {
     console.log("Server says: ", data);
