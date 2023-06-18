ffecf7856e4c565565fb73df008c10c5ff2f44be
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-18 00:55:35 -0600

    WASD keys passed and command strings connected

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
-const setupInput = function() {
+// Stores the active TCP connection object
+let connection;
 
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +13,15 @@ const setupInput = function() {
     stdin.on("data", (key) => {
       if (key === '\u0003') {
         process.exit();
+        // Additional logic for handling "wasd" key press to move
+      } else if (key === 'w') {
+        connection.write("Move: up");
+      } else if (key === 'a') {
+        connection.write("Move: left");
+      } else if (key === 's') {
+        connection.write("Move: down");
+      } else if (key === 'd') {
+        connection.write("Move: right");
       }
     });
   };
