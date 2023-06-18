d519838c7dbc0ed7b2c8dd9cb5781de5db3f0f50
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-17 18:26:29 -0600

    setup input and handleUserInput

--- play.js
+++ play.js
@@ -1,2 +1,18 @@
 const connect = require("./client.js");
 
+const setupInput = function() {
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+
+  };
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
